2b499165da8f48fd2294d67ce3d0779e0a43c656
wends05 <cxnner05@gmail.com>
2024-11-05 23:47:48 +0800

    [Started] About page layout of elements and added reference header sizes

---
+++ src/components/Track.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Track = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div className="flex flex-col text-start p-3  rounded-xl bg-violet-950 min-h-40 min-w-72">
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Track;
