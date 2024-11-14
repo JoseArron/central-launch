7fe1f9177ae4a8c31a10c179fb50c88028319c98
wends05 <cxnner05@gmail.com>
2024-11-14 14:01:08 +0800

    remove padding from Home Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import FAQs from "@/components/sections/FAQs";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full flex flex-col px-8 lg:px-12 xl:px-16">
+    <div className="min-h-screen w-full flex flex-col">
       <Header />
       <Landing />
       <About />
