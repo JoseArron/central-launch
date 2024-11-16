e9aa803802b5b963225b1edee00623e3fd49af46
wends05 <cxnner05@gmail.com>
2024-11-16 15:28:17 +0800

    added a dark leaf, adjusted rotatons and motion values

--- src/components/sections/FAQs.tsx
+++ src/components/sections/FAQs.tsx
@@ -1,7 +1,7 @@
 "use client";
 import FAQ from "../FAQ";
 import { motion, useScroll, useTransform } from "motion/react";
-import Leaf from "../Leaf";
+import Leaf, { LeafType } from "../Leaf";
 import { useRef } from "react";
 
 type FaqsProps = {
@@ -34,10 +34,10 @@ export default function FAQs() {
     offset: ["start end", "end start"],
   });
 
-  const leafGroup1Y = useTransform(scrollYProgress, [0, 1], ["0vh", "50vh"]);
+  const leafGroup1Y = useTransform(scrollYProgress, [0, 1], ["0vh", "20vh"]);
   const leafGroup2Y = useTransform(scrollYProgress, [0, 1], ['0vh', "50vh"]);
-  const leafGroup3Y = useTransform(scrollYProgress, [0, 1], ["0vh", "50vh"]);
-  const leafGroup4Y = useTransform(scrollYProgress, [0, 1], ["0vh", "100vh"]);
+  const leafGroup3Y = useTransform(scrollYProgress, [0, 1], ["0vh", "40vh"]);
+  const leafGroup4Y = useTransform(scrollYProgress, [0, 1], ["0vh", "90vh"]);
 
   return (
     <div
@@ -48,19 +48,19 @@ export default function FAQs() {
       <div className="-z-10 relative w-full">
         <motion.div style={{ y: leafGroup1Y }}>
           <Leaf className={"w-20 h-20 left-32 rotate-12"} />
-          <Leaf className={"w-20 h-20 right-32 bottom-0 -rotate-45"} />
+          <Leaf className={"w-20 h-20 right-32 bottom-0 -rotate-45"} leafType={LeafType.dark} />
         </motion.div>
         <motion.div style={{ y: leafGroup2Y }}>
           <Leaf className={"w-20 h-20 right-10 bottom-0 rotate-12"} />
-          <Leaf className={"w-20 h-20 left-0 top-0 rotate-12"} />
+          <Leaf className={"w-20 h-20 left-0 top-0 "} />
         </motion.div>
         <motion.div style={{ y: leafGroup3Y }}>
-          <Leaf className={"w-20 h-20 right-20 rotate-12"} />
-          <Leaf className={"w-20 h-20 left-48 top-10 rotate-12"} />
+          <Leaf className={"w-20 h-20 right-24 -rotate-12"} leafType={LeafType.dark} />
+          <Leaf className={"w-20 h-20 left-48 top-10 -rotate-45"} leafType={LeafType.dark} />
         </motion.div>
         <motion.div style={{ y: leafGroup4Y }}>
-          <Leaf className={"w-20 h-20 right-20 rotate-12"} />
-          <Leaf className={"w-20 h-20 left-48 top-10 rotate-12"} />
+          <Leaf className={"w-20 h-20 right-20 -rotate-45"} />
+          <Leaf className={"w-20 h-20 left-20 top-10 rotate-45"} leafType={LeafType.dark} />
         </motion.div>
       </div>
       <h1>FAQs</h1>
