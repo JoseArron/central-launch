c8b54d4d1f5a81e5968ce2ec665675fd76f73c4b
wends05 <cxnner05@gmail.com>
2024-11-16 11:30:07 +0800

    added fade in animations

--- src/components/sections/MadePossible.tsx
+++ src/components/sections/MadePossible.tsx
@@ -1,5 +1,7 @@
-import Helpers from "../Helpers";
+"use client";
 
+import Helpers from "../Helpers";
+import { motion } from "motion/react";
 const organizers = [
   {
     logoSrc: "",
@@ -43,53 +45,125 @@ const partners = [
   {
     logoSrc: "",
     title: "Sample Partner",
-  },{
+  },
+  {
     logoSrc: "",
     title: "Sample Partner",
-  },{
+  },
+  {
     logoSrc: "",
     title: "Sample Partner",
-  },{
+  },
+  {
     logoSrc: "",
     title: "Sample Partner",
-  },{
+  },
+  {
     logoSrc: "",
     title: "Sample Partner",
-  },{
+  },
+  {
     logoSrc: "",
     title: "Sample Partner",
-  },{
+  },
+  {
     logoSrc: "",
     title: "Sample Partner",
   },
 ];
 
 export default function MadePossible() {
+  const appearVariants = {
+    visible: {
+      opacity: 100,
+      y: 0,
+    },
+    hidden: {
+      opacity: 0,
+      y: 100,
+    },
+  };
   return (
-    <div className="flex flex-col items-center justify-center py-12 px-4 md:px-12 lg:px-52 gap-14">
+    <div className="flex flex-col items-center justify-center py-12 px-20 md:px-24 lg:px-52 gap-14">
       <div className="w-full space-y-2">
         <h1 className="text-center">Organizers</h1>
-        <div className="grid lg:grid-cols-3 w-full gap-10 grid-cols-2">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          transition={{
+            staggerChildren: 0.2,
+            ease: "linear",
+          }}
+          variants={appearVariants}
+          className="grid lg:grid-cols-3 w-full gap-10 grid-cols-1 text-white"
+        >
           {organizers.map(({ logoSrc, title }, idx) => (
-            <Helpers logosrc={logoSrc} title={title} key={idx} />
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              variants={appearVariants}
+              key={idx}
+            >
+              <Helpers logosrc={logoSrc} title={title} />
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
       <div className="w-full space-y-2">
         <h1 className="text-center">Sponsors</h1>
-        <div className="grid lg:grid-cols-3 w-full gap-10 grid-cols-2">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          transition={{
+            staggerChildren: 0.2,
+            ease: "linear",
+          }}
+          variants={appearVariants}
+          className="grid lg:grid-cols-3 w-full gap-10 grid-cols-1 text-white"
+        >
           {sponsors.map(({ logoSrc, title }, idx) => (
-            <Helpers logosrc={logoSrc} title={title} key={idx} />
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              transition={{
+                staggerChildren: 0.2,
+                ease: "linear",
+              }}
+              variants={appearVariants}
+              key={idx}
+            >
+              <Helpers logosrc={logoSrc} title={title} />
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
       <div className="w-full space-y-2">
         <h1 className="text-center">Partners</h1>
-        <div className="grid lg:grid-cols-3 w-full gap-10 grid-cols-2">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          transition={{
+            staggerChildren: 0.2,
+            ease: "linear",
+          }}
+          variants={appearVariants}
+          className="grid lg:grid-cols-3 w-full gap-10 grid-cols-1 text-white"
+        >
           {partners.map(({ logoSrc, title }, idx) => (
-            <Helpers logosrc={logoSrc} title={title} key={idx} />
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              transition={{
+                staggerChildren: 0.2,
+                ease: "linear",
+              }}
+              variants={appearVariants}
+              key={idx}
+            >
+              <Helpers logosrc={logoSrc} title={title} />
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </div>
   );
