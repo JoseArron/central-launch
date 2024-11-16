879501d3db10416099eff02a085d22a9532ce68a
Jose <joseearronn@gmail.com>
2024-11-16 21:37:47 +0800

    [Fix] small fix

--- src/components/Speaker.tsx
+++ src/components/Speaker.tsx
@@ -10,9 +10,9 @@ interface SpeakerProps {
 const Speaker = ({ imgSrc, name, reverse = true }: SpeakerProps) => {
   return (
     <div
-      className={`flex ${reverse ? "flex-row-reverse" : "flex-row"} w-full h-fit gap-8 min-h-40 items-center`}
+      className={`flex ${reverse ? "flex-row-reverse text-end" : "flex-row text-start"} w-full h-fit gap-8 min-h-40 items-center `}
     >
-      <div className="bg-blue rounded-full w-24 h-24 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-[34rem] lg:h-[33rem] aspect-square"></div>
+      <div className="bg-blue rounded-full size-40 sm:size-48 md:size-64 lg:w-[24rem] lg:h-[23rem] aspect-square"></div>
       <WavyText text={name} className="text-3xl lg:text-4xl break-words" />
     </div>
   );
