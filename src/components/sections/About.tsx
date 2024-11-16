8a0ecd7a405d90a9bbe11b5360469bb7a7ea1ef3
wends05 <cxnner05@gmail.com>
2024-11-16 12:50:25 +0800

    added clouds

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -4,44 +4,69 @@ import Image from "next/image";
 import Circle from "../Circle";
 import Track from "../Track";
 import { motion } from "motion/react";
+import cloud from "@/../public/assets/cloud.svg";
 
+const appearVariants = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 100,
+  },
+};
+
+const appearStagger = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 100,
+  },
+  transition: {
+    staggerChildren: 0.2,
+  },
+};
 export default function About() {
-  const appearVariants = {
-    hidden: {
-      y: 100,
-      opacity: 0,
-    },
-    visible: {
-      y: 0,
-      opacity: 100,
-    },
-  };
-
-  const appearStagger = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 100,
-    },
-    transition: {
-      staggerChildren: 0.2,
-    },
-  };
-
-  const appearStaggerChildren = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 100,
-    },
-  };
   return (
     <div
       id="about"
-      className="bg-white flex flex-col items-center justify-center py-12 px-4 md:px-12 lg:px-24 xl:px-36 gap-20 z-10 w-full"
+      className="bg-gray-200 flex flex-col items-center justify-center py-12 px-4 md:px-12 lg:px-24 xl:px-36 gap-20 z-10 w-full relative"
+ 
     >
+      <div className="-z-10">
+        <div>
+          <div className="absolute w-40 h-40 md:w-40 md:h-40 left-10 top-0 md:left-32">
+            <Image fill src={cloud} alt="img" className="object-contain" />
+          </div>
+          <div className="absolute w-[20vw] h-[20vh] top-20 left-20 md:left-40">
+            <Image fill src={cloud} alt="img" className="object-contain" />
+          </div>
+          <div className="absolute w-96 h-96 top-24 right-20">
+            <Image fill src={cloud} alt="img" className="object-contain" />
+          </div>
+        </div>
+        <div>
+          <div className="absolute w-64 h-64 top-1/2 -translate-y-1/2">
+            <Image fill src={cloud} alt="img" className="object-contain" />
+          </div>
+          <div className="absolute w-64 h-64 top-[40%] left-20 -translate-y-1/2">
+            <Image fill src={cloud} alt="img" className="object-contain" />
+          </div>
+        </div>
+        <div className="hidden md:block">
+          <div className="absolute w-96 h-96 bottom-72 right-20">
+            <Image fill src={cloud} alt="img" className="object-contain" />
+          </div>
+          <div className="absolute w-96 h-96 bottom-20 left-20">
+            <Image fill src={cloud} alt="img" className="object-contain" />
+          </div>
+          <div className="absolute w-96 h-96 -bottom-20 left-32">
+            <Image fill src={cloud} alt="img" className="object-contain" />
+          </div>
+        </div>
+      </div>
       <motion.div
         initial="hidden"
         whileInView="visible"
@@ -81,32 +106,32 @@ export default function About() {
               }}
               viewport={{ once: true }}
             >
-              <motion.div variants={appearStaggerChildren}>
+              <motion.div variants={appearStagger}>
                 <Circle className="absolute w-24 h-24 top-12 -left-28">
                   <p>Food</p>
                 </Circle>
               </motion.div>
-              <motion.div variants={appearStaggerChildren}>
+              <motion.div variants={appearStagger}>
                 <Circle className="absolute -left-16 bottom-0">
                   <p>Pitching</p>
                 </Circle>
               </motion.div>
-              <motion.div variants={appearStaggerChildren}>
+              <motion.div variants={appearStagger}>
                 <Circle className="absolute -left-2 -bottom-40 w-36 h-36">
                   <p>Connections</p>
                 </Circle>
               </motion.div>
-              <motion.div variants={appearStaggerChildren}>
+              <motion.div variants={appearStagger}>
                 <Circle className="absolute -right-10 -bottom-36 w-36 h-36">
                   <p>Networks</p>
                 </Circle>
               </motion.div>
-              <motion.div variants={appearStaggerChildren}>
+              <motion.div variants={appearStagger}>
                 <Circle className="absolute -right-24 bottom-5 w-24 h-24">
                   <p>Workshops</p>
                 </Circle>
               </motion.div>
-              <motion.div variants={appearStaggerChildren}>
+              <motion.div variants={appearStagger}>
                 <Circle className="absolute -right-24 top-5">
                   <p>Swag</p>
                 </Circle>
