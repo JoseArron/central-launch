859bdceeffca011848fce873b18e99432a8bb011
Jose <joseearronn@gmail.com>
2024-11-17 16:47:42 +0800

    [Add] Add footer

--- src/components/Speaker.tsx
+++ src/components/Speaker.tsx
@@ -13,7 +13,10 @@ const Speaker = ({ imgSrc, name, reverse = true }: SpeakerProps) => {
       className={`flex ${reverse ? "flex-row-reverse text-end" : "flex-row text-start"} w-full h-fit gap-8 min-h-40 items-center `}
     >
       <div className="bg-blue rounded-full size-40 sm:size-48 md:size-64 lg:w-[24rem] lg:h-[23rem] aspect-square"></div>
-      <WavyText text={name} className="text-3xl lg:text-4xl break-words" />
+      <WavyText
+        text={name}
+        className="text-2xl md:text-3xl lg:text-4xl break-words"
+      />
     </div>
   );
 };
